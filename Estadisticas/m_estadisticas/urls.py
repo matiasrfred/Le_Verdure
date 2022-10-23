5772d7de675b569fd7bc6d86ef87146ba8fcaaf7
vi.navarro <vi.navarro@duocuc.cl>
2022-10-23 15:25:45 -0400

    creacion de estadisticas api

--- Estadisticas/m_estadisticas/urls.py
+++ Estadisticas/m_estadisticas/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import estadisticaView
+from .views import estadisticaView, EstadisticasAllViewset, EstadisticasViewset
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register('estadisticas',EstadisticasViewset,EstadisticasAllViewset)
+router.register('estadisticas_all',EstadisticasAllViewset)
 
 urlpatterns=[
     path('estadistica/', estadisticaView.as_view(), name='estadistica_list'),
-    path('estadistica/<int:id>', estadisticaView.as_view(), name='estadistica_proces')
+    path('estadistica/<int:id_estad>', estadisticaView.as_view(), name='estadistica_proces')
 ]
